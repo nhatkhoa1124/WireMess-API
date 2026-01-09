cfc37a2353d9b2d4fda887be52dcffd0a70f1d8a
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-01-09 17:38:58 +0700

    feat: Add User extensions.

---
+++ WireMess/Utils/Extensions/UserExtensions.cs
@@ -0,0 +1,23 @@
+﻿using WireMess.Models.DTOs.Response.User;
+using WireMess.Models.Entities;
+
+namespace WireMess.Utils.Extensions
+{
+    public static class UserExtensions
+    {
+        public static UserDto MapUserToDto(this User user)
+        {
+            if (user == null) return null;
+
+            return new UserDto
+            {
+                Username = user.Username,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                IsOnline = user.IsOnline,
+                LastActive = user.LastActive,
+                AvatarUrl = user.AvatarUrl
+            };
+        }
+    }
+}
