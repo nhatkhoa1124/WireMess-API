47c12fb21e036f29ab776377982d604e8965a843
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-01-10 18:28:04 +0700

    refactor: Change to use constant seed for DateTime.

--- WireMess/Data/AppDbContext.cs
+++ WireMess/Data/AppDbContext.cs
@@ -27,22 +27,25 @@ namespace WireMess.Data
             modelBuilder.Entity<Message>().HasQueryFilter(m => !m.IsDeleted);
             modelBuilder.Entity<Attachment>().HasQueryFilter(a => !a.IsDeleted);
             modelBuilder.Entity<Conversation>().HasQueryFilter(c => !c.IsDeleted);
+            modelBuilder.Entity<UserConversation>().HasQueryFilter(uc => !uc.IsDeleted);
+
+            var seedDate = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             modelBuilder.Entity<ConversationType>().HasData(
             new ConversationType
             {
                 Id = (int)ConversationTypeEnum.Direct,
                 TypeName = nameof(ConversationTypeEnum.Direct),
                 Description = "One-on-one chat",
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = DateTime.UtcNow
+                CreatedAt = seedDate,
+                UpdatedAt = seedDate
             },
             new ConversationType
             {
                 Id = (int)ConversationTypeEnum.Group,
                 TypeName = nameof(ConversationTypeEnum.Group),
                 Description = "Multi-user chat",
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = DateTime.UtcNow
+                CreatedAt = seedDate,
+                UpdatedAt = seedDate
             });
         }
 
