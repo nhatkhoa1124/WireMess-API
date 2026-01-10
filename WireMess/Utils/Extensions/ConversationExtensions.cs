f3889ae1434a7c38abb0ea9d8eb204313da54e32
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-01-10 15:07:39 +0700

    feat: Add Conversation Extension.

---
+++ WireMess/Utils/Extensions/ConversationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using WireMess.Models.DTOs.Response.Conversation;
+using WireMess.Models.DTOs.Response.User;
+using WireMess.Models.Entities;
+
+namespace WireMess.Utils.Extensions
+{
+    public static class ConversationExtensions
+    {
+        public static ConversationDto MapConversationToDto(this Conversation conversation)
+        {
+            if (conversation == null) return null;
+
+            return new ConversationDto
+            {
+                Id = conversation.Id,
+                ConversationName = conversation.ConversationName,
+                LastMessageAt = conversation.LastMessageAt,
+                AvatarUrl = conversation.AvatarUrl
+            };
+        }
+    }
+}
