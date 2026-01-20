03700d51b13e936dc3113b762783d2211023c4db
Nhật Khoa Trần Nguyễn <nhatkhoa1124@gmail.com>
2026-01-20 16:04:20 +0700

    feat: Add xUnit test module.

---
+++ WireMess.Test/Fixtures/DatabaseFixture.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WireMess.Data;
+
+namespace WireMess.Test.Fixtures
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public AppDbContext Context { get; private set; }
+
+        public DatabaseFixture()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            Context = new AppDbContext(options);
+            Context.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+            Context.Dispose();
+        }
+    }
+}
